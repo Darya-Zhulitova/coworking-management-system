d8972d876441f4dbe74e6ee22e556eda1ca9cc77
Darya-Zhulitova <154079181+Darya-Zhulitova@users.noreply.github.com>
2026-02-03 17:12:18 +0300

    add domain entities for membership and request

---
+++ user-service/src/main/java/com/hse/userservice/domain/request/RequestStatus.java
@@ -0,0 +1,7 @@
+package com.hse.userservice.domain.request;
+
+public enum RequestStatus {
+    CREATED,
+    APPROVED,
+    REJECTED
+}
---
+++ user-service/src/main/java/com/hse/userservice/domain/request/ServiceRequestStatus.java
@@ -0,0 +1,8 @@
+package com.hse.userservice.domain.request;
+
+public enum ServiceRequestStatus {
+    NEW,
+    IN_PROGRESS,
+    COMPLETED,
+    REJECTED
+}
